a53334f956394239b5be3c0c2fcad268934a3b6c
HaiNT <thanhhaitk25@gmail.com>
2026-07-14 21:19:39 +0700

    Feat: Add confirm password

--- js/login.js
+++ js/login.js
@@ -1,14 +1,46 @@
-const password = document.getElementById("password");
-const toggle = document.getElementById("togglePassword");
+const toggles = document.querySelectorAll(".togglePassword");
 
-toggle.addEventListener("click", () => {
-    if(password.type === "password"){
-        password.type = "text";
-        toggle.textContent = "HIDE";
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    const input = toggle.parentElement.querySelector("input");
+
+    if (input.type === "password") {
+      input.type = "text";
+      toggle.textContent = "HIDE";
+    } else {
+      input.type = "password";
+      toggle.textContent = "SHOW";
     }
-    else{
-        password.type = "password";
-        toggle.textContent = "SHOW";
-    }
+  });
 });
 
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+const message = document.getElementById("message");
+
+function checkPassword() {
+  if (confirmPassword.value === "") {
+    message.textContent = "";
+    return;
+  }
+
+  if (password.value === confirmPassword.value) {
+    message.textContent = "✔ Mật khẩu khớp";
+    message.style.color = "green";
+  } else {
+    message.textContent = "✖ Mật khẩu không khớp";
+    message.style.color = "red";
+  }
+}
+
+password.addEventListener("input", checkPassword);
+confirmPassword.addEventListener("input", checkPassword);
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", (e) => {
+  if (password.value !== confirmPassword.value) {
+    e.preventDefault();
+    alert("Mật khẩu không khớp!");
+  }
+});
