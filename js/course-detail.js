a80d046bbb9dd84580bb3b0fff9923d0d5b3e4d7
HaiNT <thanhhaitk25@gmail.com>
2026-07-24 22:20:07 +0700

    Bug: Fix bug cannot access course detail

--- js/course-detail.js
+++ js/course-detail.js
@@ -1,17 +1,22 @@
-const params = new URLSearchParams(window.location.search);
+console.log("========== COURSE DETAIL ==========");
 
+const params = new URLSearchParams(window.location.search);
 const id = Number(params.get("id"));
+
+console.log("ID:", id);
+console.log("Courses:", courses);
 
 const course = courses.find((c) => c.id === id);
 
+console.log("Found:", course);
+
 if (!course) {
   document.body.innerHTML = "<h1>Course Not Found</h1>";
+  throw new Error("Course Not Found");
 }
 
 document.getElementById("courseTitle").textContent = course.title;
-
 document.getElementById("courseImage").src = course.image;
-
 document.getElementById("courseDescription").textContent = course.description;
 
 const lessonList = document.getElementById("lessonList");
